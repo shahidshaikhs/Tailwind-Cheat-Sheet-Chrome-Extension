50631b7d344120c4d37427b1f4f6d9d6f33965ce
MEDIA\shahid.sh <shahid.sh@media.net>
2022-06-19 00:39:16 +0530

    Changes to Readme.md

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ function App() {
     const [cheatSheetData, setCheatSheetData] = useState(cheatsheet);
 
     useEffect(() => {
-        document.querySelector('body').classList.add('dark');
-        document.querySelector('body').classList.add('dark:bg-zinc-900');
         document.querySelector('body').classList.add('dark:bg-zinc-900');
     }, []);
 
@@ -57,7 +55,10 @@ function App() {
                     </svg>
                     Tailwind Cheat Sheet
                 </h1>
-                <div className="flex ml-auto items-center border-zinc-700 border-2 rounded-full py-1 px-3">
+                <a href="https://github.com/shahidshaikhs/Tailwind-Cheat-Sheet-Chrome-Extension" target="_blank" className="inline-block ml-auto text-zinc-600 hover:text-sky-500 duration-200 transition">
+                    <svg fill="currentColor" className="h-6 w-6 ml-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.288-.6-1.175-1.025-1.413-.35-.187-.85-.65-.013-.662.788-.013 1.35.725 1.538 1.025.9 1.512 2.338 1.087 2.912.825.088-.65.35-1.087.638-1.337-2.225-.25-4.55-1.113-4.55-4.938 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.275.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z"/></svg>
+                </a>
+                <div className="flex items-center border-zinc-700 border-2 rounded-full py-1 px-3 ml-3">
                     <input onChange={(e) => searchClass(e.target.value)} type="text" className="bg-transparent outline-none text-white placeholder:text-zinc-600 text-sm" placeholder="Search"/>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-auto text-zinc-600" fill="none"
                          viewBox="0 0 24 24"
